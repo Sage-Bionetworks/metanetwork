cebc27befb4d04c2273b094d94289d3e6193ac29
Ben Logsdon <blogsdon@DHCP149223.FHCRC.ORG>
2015-01-09 15:41:13 -0800

    initial commit of aracne wrapper function

--- R/aracne.R
+++ R/aracne.R
@@ -1,1 +1,33 @@
 ###Function to run aracne on the data
+aracne <- function(data,path=NULL,pval=NULL){
+  #path is the a string of the path to th aracne compiled executable
+  #data is a matrix of the gene expression data of interest
+  if(!is.null(path)){
+    setwd(path)
+  }
+  if(is.null(pval)){
+    pval <- 0.05/choose(ncol(data),2)
+  }
+  
+  dataMatrix <- cbind(rownames(data),rownames(data),data)
+  colnames(dataMatrix) <- c('name1','name2',colnames(data))
+  write.table(dataMatrix,file='dataMatrix.tsv',sep='\t',quote=FALSE,row.names=FALSE)
+  
+  command_string <- paste('./aracne2 -i dataMatrix.tsv -a adaptive_partitioning -p ',pval,' -o result.out',sep='')
+  system(command_string) 
+  
+  
+  
+}
+
+n <- 100
+m <- 100
+
+dum <- matrix(rnorm(n*m),n,m)
+colnames(dum) <- paste('a',1:100,sep='')
+rownames(dum) <- paste('b',1:100,sep='')
+#dum <- cbind(rownames(dum),dum)
+aracne(data=dum)
+write.table(dum,file='dum.txt',sep='\t',quote=FALSE)
+system('./aracne2 -i dum.txt -a adaptive_partitioning -p 1e-3')
+system('./aracne2 -i test/arraydata10x336.exp -a adaptive_partitioning -p 1e-3')
