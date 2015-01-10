a160af7d0d29488d5a1b27c9bc57d6c2da77defb
Ben Logsdon <blogsdon@DHCP149223.FHCRC.ORG>
2015-01-09 16:12:16 -0800

    first pass of finished version of aracne function, yay!

--- R/aracne.R
+++ R/aracne.R
@@ -15,19 +15,37 @@ aracne <- function(data,path=NULL,pval=NULL){
   
   command_string <- paste('./aracne2 -i dataMatrix.tsv -a adaptive_partitioning -p ',pval,' -o result.out',sep='')
   system(command_string) 
-  
-  
-  
+  result <- readLines('result.out')
+  network <- matrix(0,nrow(data),nrow(data))
+  rownames(network) <- rownames(data)
+  colnames(network) <- rownames(data)
+  fun <- function(x,ref){
+    vec <- strsplit(x,'\t')[[1]]
+    model <-list();
+    model$gene <- vec[1];
+    model$vec <- rep(0,length(ref));
+    names(model$vec) <- ref;
+    model$keepGene <- vec[-1][1:length(vec[-1])%%2==1];
+    model$weights <- vec[-1][1:length(vec[-1])%%2==0];
+    model$vec[model$keepGene] <- model$weights
+    model$vec <- as.numeric(model$vec)
+    return(model)
+  }
+  resultFormat <- lapply(result[-c(1:18)],fun,ref=rownames(data))
+  for (i in 1:length(resultFormat)){
+    network[resultFormat[[i]]$gene,]<-resultFormat[[i]]$vec;
+  }
+  return(network)
 }
 
-n <- 100
+n <- 20
 m <- 100
 
 dum <- matrix(rnorm(n*m),n,m)
-colnames(dum) <- paste('a',1:100,sep='')
-rownames(dum) <- paste('b',1:100,sep='')
+colnames(dum) <- paste('a',1:m,sep='')
+rownames(dum) <- paste('b',1:n,sep='')
 #dum <- cbind(rownames(dum),dum)
-aracne(data=dum)
+a<-aracne(data=dum,pval = 0.1)
 write.table(dum,file='dum.txt',sep='\t',quote=FALSE)
 system('./aracne2 -i dum.txt -a adaptive_partitioning -p 1e-3')
 system('./aracne2 -i test/arraydata10x336.exp -a adaptive_partitioning -p 1e-3')
