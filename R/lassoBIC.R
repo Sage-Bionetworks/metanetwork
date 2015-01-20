8cc55103175a4d38a5767fb1e7dd098dee78fa67
Ben Logsdon <blogsdon@DHCP149223.FHCRC.ORG>
2015-01-20 10:30:58 -0800

    Fix nomenclature bug

--- R/lassoBIC.R
+++ R/lassoBIC.R
@@ -1,11 +1,11 @@
 #function to grab BIC solution for lasso
 lassoBIC <- function(y,x){
   require(glmnet)
-  res <- glmnet(y=y,x=X)
-  resid <- y - X%*%res$beta;
+  res <- glmnet(y=y,x=x)
+  resid <- y - x%*%res$beta;
   error <- apply(resid^2,2,mean)
   nonzero <- apply(res$beta,2,function(x) sum(x!=0))
-  n <- nrow(X)
+  n <- nrow(x)
   bic <- n*log(error)+nonzero*log(n)
   return(res$beta[,which.min(bic)])
 }
