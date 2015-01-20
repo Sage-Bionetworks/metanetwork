4a0aff4d92a02677098bd3edb8a3b8ab3f5cba9c
Ben Logsdon <blogsdon@DHCP149223.FHCRC.ORG>
2015-01-20 09:04:04 -0800

    updated genie3 function

--- R/genie3.R
+++ R/genie3.R
@@ -1,1 +1,9 @@
 ###Function to run genie3 on the data
+genie3 <- function(y,x){
+  #function to run genie3 on a single gene
+  #IncNodePurity
+  require(randomForest)
+  rf <- randomForest(x,y,mtry=round(sqrt(ncol(x))),ntree=1000,importance=TRUE)
+  im <- importance(rf)[,'IncNodePurity'];
+  return(im)
+}
