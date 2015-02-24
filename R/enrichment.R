f919d07c46d36fed35c3a4421a71efb5b75c1c02
Ben Logsdon <aeberhart@gmail.com>
2015-02-24 12:31:06 -0800

    combine into one fxn

--- R/enrichment.R
+++ R/enrichment.R
@@ -1,5 +1,5 @@
 #function to run simple enrichments
-computeFisherEnrichment <- function(list1,list2,list3){
+enrichment <- function(list1,list2,list3){
   n2 <- length(list3);
   n1 <- length(list1);
   k <- length(list2);
@@ -13,26 +13,6 @@ computeFisherEnrichment <- function(list1,list2,list3){
   
   #cat(n2,n1,a1,m,n,s1)
   enr <- (s1/(k))/(m/n2)
-  return(enr);
-  
-}
-
-computeFisherPvalue <- function(list1,list2,list3){
-  n2 <- length(list3);
-  n1 <- length(list1);
-  k <- length(list2);
-  a1 <- intersect(list1,list3);
-  m <- length(a1);
-  n <- n2-m;
-  #cat(n2,n1,a1,m,n,'\n')
-  
-  #s1 <- unlist(lapply(1:n2,getSum,list2,list1));
-  s1<-sum(list2%in%list1);
-  
-  #cat(n2,n1,a1,m,n,s1)
-  #model <- list();
   pval <- phyper(q=s1-1,m=m,n=n,k=k,lower.tail=F);
-  #model$enr <- (s1/(k))/(m/n2)
-  return(pval);
-  
+  return(list(enr=enr,pval=pval)); 
 }
