1761dae9ee2f76017936659d23950312504da56e
Ben Logsdon <aeberhart@gmail.com>
2015-04-06 11:30:09 -0700

    fix bug in soft thresholding fxn

--- R/wgcna.R
+++ R/wgcna.R
@@ -1,7 +1,14 @@
 # ###Function to run wgcna on the data
 wgcna <- function(data,path=NULL){
   library(WGCNA)
-  softThreshold = pickSoftThreshold(data)$powerEstimate
-  network <- abs(cor)^softThreshold
+  softThresholdMatrix = pickSoftThreshold(data)
+  if(!is.na(softThresholdMatrix$powerEstimate)){
+    softThreshold = softThresholdMatrix$powerEstimate
+  }else{
+    w1 <- which.max(softThresholdMatrix$fitIndices$SFT.R.sq)
+    softThreshold = softThresholdMatrix$fitIndices$Power[w1]
+    cat('warning, r^2 less than .85:',softThresholdMatrix$fitIndices$SFT.R.sq[w1])
+  }
+  network <- abs(cor(data))^softThreshold
   save(network,file='result_wgcna.rda')
 }
