6683b22a7b6f6576a9cd32af8fee23010b51628c
Ben Logsdon <aeberhart@gmail.com>
2015-04-06 11:31:55 -0700

    fix bugs

--- R/wgcna.R
+++ R/wgcna.R
@@ -7,7 +7,7 @@ wgcna <- function(data,path=NULL){
   }else{
     w1 <- which.max(softThresholdMatrix$fitIndices$SFT.R.sq)
     softThreshold = softThresholdMatrix$fitIndices$Power[w1]
-    cat('warning, r^2 less than .85:',softThresholdMatrix$fitIndices$SFT.R.sq[w1])
+    cat('warning, r^2 less than .85:',softThresholdMatrix$fitIndices$SFT.R.sq[w1],'\n')
   }
   network <- abs(cor(data))^softThreshold
   save(network,file='result_wgcna.rda')
