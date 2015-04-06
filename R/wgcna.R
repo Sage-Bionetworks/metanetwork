5ace1543328169140d77e18f3d4504ffa6fecb78
Ben Logsdon <aeberhart@gmail.com>
2015-04-06 11:20:22 -0700

    fix wgcna bug

--- R/wgcna.R
+++ R/wgcna.R
@@ -1,6 +1,6 @@
 # ###Function to run wgcna on the data
-library(WGCNA)
 wgcna <- function(data,path=NULL){
+  library(WGCNA)
   softThreshold = pickSoftThreshold(data)$powerEstimate
   network <- abs(cor)^softThreshold
   save(network,file='result_wgcna.rda')
