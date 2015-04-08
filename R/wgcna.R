f2ab2a32b034340877b33cab8c71a465abafc8ce
Ben Logsdon <aeberhart@gmail.com>
2015-04-08 13:18:11 -0700

    change soft thresholding to hard thresholding

--- R/wgcna.R
+++ R/wgcna.R
@@ -1,14 +1,14 @@
 # ###Function to run wgcna on the data
 wgcna <- function(data,path=NULL){
   library(WGCNA)
-  softThresholdMatrix = pickSoftThreshold(data)
-  if(!is.na(softThresholdMatrix$powerEstimate)){
-    softThreshold = softThresholdMatrix$powerEstimate
+  hardThresholdMatrix = pickHardThreshold(data)
+  if(!is.na(hardThresholdMatrix$cutEstimate)){
+    hardThreshold = hardThresholdMatrix$cutEstimate
   }else{
-    w1 <- which.max(softThresholdMatrix$fitIndices$SFT.R.sq)
-    softThreshold = softThresholdMatrix$fitIndices$Power[w1]
-    cat('warning, r^2 less than .85:',softThresholdMatrix$fitIndices$SFT.R.sq[w1],'\n')
+    w1 <- which.max(hardThresholdMatrix$fitIndices$SFT.R.sq)
+    hardThreshold = hardThresholdMatrix$fitIndices$Cut[w1]
+    cat('warning, r^2 less than .85:',hardThresholdMatrix$fitIndices$SFT.R.sq[w1],'\n')
   }
-  network <- abs(cor(data))^softThreshold
+  network <- abs(cor(data))>hardThreshold
   save(network,file='result_wgcna.rda')
 }
