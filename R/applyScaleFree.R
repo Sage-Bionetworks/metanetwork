6cb6681473de3416eb1633833c5735533b9122f5
Ben Logsdon <aeberhart@gmail.com>
2015-04-09 12:07:02 -0700

    scale free network extraction

---
+++ R/applyScaleFree.R
@@ -0,0 +1,20 @@
+applyScaleFree <- function(network){
+  require(WGCNA)
+  network <- network/2 + t(network)/2
+  network <- as.matrix(network)
+  edgeVec <- network[which(upper.tri(network))]
+  edgeVec <- abs(edgeVec)
+  edgeVec <- edgeVec[which(edgeVec!=0)]
+  cutPoints <- quantile(edgeVec,seq=(0,1,length.out=17))
+  diag(network) <- 1
+  hardThresholdMatrix <- pickHardThreshold.fromSimilarity(similarity = network, cutVector = cutPoints)
+  if(!is.na(hardThresholdMatrix$cutEstimate)){
+    hardThreshold = hardThresholdMatrix$cutEstimate
+  }else{
+    w1 <- which.max(hardThresholdMatrix$fitIndices$SFT.R.sq)
+    hardThreshold = hardThresholdMatrix$fitIndices$Cut[w1]
+    cat('warning, r^2 less than .85:',hardThresholdMatrix$fitIndices$SFT.R.sq[w1],'\n')
+  }
+  network <- abs(network) > hardThreshold
+  return(network)
+}
