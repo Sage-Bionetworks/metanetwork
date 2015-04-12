29b1af440fdf29b2f18488dcf6f8a573fa233bab
Ben Logsdon <aeberhart@gmail.com>
2015-04-12 12:01:08 -0700

    first pass of network statistic function

--- R/networkStatistics.R
+++ R/networkStatistics.R
@@ -1,8 +1,14 @@
-#function to extract network statistics from data
-#degree
-#stress
-#betweeness centrality
-#topological coefficient
-#shared neighbors
-#clustering coefficient
-#closeness centrality
+
+networkStatistics <- function(network){
+  #function to extract network statistics from data
+  #degree
+  #stress
+  #betweeness centrality
+  #topological coefficient
+  #shared neighbors
+  #clustering coefficient
+  #closeness centrality
+  netStat <- list()
+  netStat$hubs <- rowSums(network)
+  return(netStat)  
+}
