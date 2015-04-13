f9943f614110de176601368521479cd323ed3e64
Ben Logsdon <aeberhart@gmail.com>
2015-04-13 13:21:01 -0700

    some updates

--- R/makeSparseNetworkFile.R
+++ R/makeSparseNetworkFile.R
@@ -2,5 +2,5 @@ makeSparseNetworkFile <- function(network,geneId,file){
   network <- network*upper.tri(network)
   w1 <- which(network==1,arr.ind = TRUE)
   mat <- cbind(geneId[w1[,1]],geneId[w1[,2]])
-  write.csv(mat,file=file,row.names=F,col.names=F)
+  write.table(mat,file=file,row.names=FALSE,col.names=FALSE,quote=FALSE,sep=',')
 }
