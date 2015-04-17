b55e24842cb2873b72104538a650776b5c5bb67b
Ben Logsdon <aeberhart@gmail.com>
2015-04-17 11:26:47 -0700

    dplyr-ize sparrow bonferroni

--- R/applySparrowBonferroni.R
+++ R/applySparrowBonferroni.R
@@ -1,7 +1,10 @@
 applySparrowBonferroni <- function(network){
+  require(dplyr)
   network <- network/2 + t(network)/2
-  network <- as.matrix(network)
-  thres <- qchisq(0.05/choose(nrow(network),2),1,lower.tail=F)
+  #network <- as.matrix(network)
+  network <- network %>% as.matrix
+  #thres <- qchisq(0.05/choose(nrow(network),2),1,lower.tail=F)
+  thres <- (0.05/(nrow(network) %>% choose(2))) %>% qchisq(1,lower.tail=F)
   network <- network^2>thres
   return(network)
 }
