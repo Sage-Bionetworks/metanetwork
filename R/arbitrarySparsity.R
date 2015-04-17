d22ec49d1910ea986d9e9a837b91ff12c6f8b6e9
Ben Logsdon <aeberhart@gmail.com>
2015-04-17 16:10:55 -0700

    fix arbitrary sparsity fxn

--- R/arbitrarySparsity.R
+++ R/arbitrarySparsity.R
@@ -1,9 +1,10 @@
 arbitrarySparsity <- function(network,nedges){
   require(dplyr)
+  network <- network %>% as.matrix
   diag(network) <- 0
   network <- network %>% abs
   require(dplyr)
-  networkVec <- c(network[network %>% upper.tri() %>% which])
+  networkVec <- network[network %>% upper.tri %>% which] %>% c
   sortedNetworkVec<-sort(networkVec,decreasing=T)
   thres <- sortedNetworkVec[nedges]
   return(network>thres)
