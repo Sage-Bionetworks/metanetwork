568fb3f685b567ecee3d0409308aa699bb0fb437
Ben Logsdon <aeberhart@gmail.com>
2015-04-17 11:08:18 -0700

    note to self, order of operations: %>% precedes multiplication

--- R/fastlm.R
+++ R/fastlm.R
@@ -7,7 +7,7 @@ fastlm <- function(y,x){
   Xhat <- ginv%*%t(X);
   betahat <- Xhat%*%y;
   sig <- mean((y-X%*%betahat)^2)*((n1)/(n1-ncol(X)));
-  zval <- betahat/(sig*diag(ginv) %>% sqrt());
+  zval <- betahat/((sig*diag(ginv)) %>% sqrt());
   #print('In cleaning')
   return(zval[-1]);
 }
