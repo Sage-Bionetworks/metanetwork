3ddb647208f90f5f5194986771e20267e6968c88
Ben Logsdon <aeberhart@gmail.com>
2015-04-17 10:38:00 -0700

    dplyr fastlm

--- R/fastlm.R
+++ R/fastlm.R
@@ -1,12 +1,13 @@
 fastlm <- function(y,x){
+  require(dplyr)
   X <- as.matrix(x)
   n1 <- nrow(X)
   X <- cbind(rep(1,n1),X);
-  ginv <- solve(t(X)%*%X);
+  ginv <- t(X)%*%X %>% solve();
   Xhat <- ginv%*%t(X);
   betahat <- Xhat%*%y;
   sig <- mean((y-X%*%betahat)^2)*((n1)/(n1-ncol(X)));
-  zval <- betahat/sqrt(sig*diag(ginv));
+  zval <- betahat/(sig*diag(ginv) %>% sqrt());
   #print('In cleaning')
   return(zval[-1]);
 }
