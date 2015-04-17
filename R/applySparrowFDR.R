3d453eba2f0e741b02a1e8600e4f079e5c9ddd16
Ben Logsdon <aeberhart@gmail.com>
2015-04-17 11:28:55 -0700

    dplyr-ize applySparrowFDR.R

--- R/applySparrowFDR.R
+++ R/applySparrowFDR.R
@@ -1,12 +1,12 @@
 applySparrowFDR <- function(network){
+  require(dplyr)
   network <- network/2 + t(network)/2
-  network <- as.matrix(network)
+  network <- network %>% as.matrix
   #thres <- qchisq(0.05/choose(nrow(network),2),1,lower.tail=F)
   #network1 <- pnorm(abs(network),lower.tail=F)*2
-  require(dplyr)
-  network1 <- 2*(network %>% abs() %>% pnorm(lower.tail=F))
-  network1vec <- c(network1[which(upper.tri(network1))])
-  thres <- fdrThres(network1vec)
+  network1 <- 2*(network %>% abs %>% pnorm(lower.tail=F))
+  network1vec <- network1[network1 %>% upper.tri %>% which] %>% c
+  thres <- network1vec %>% fdrThres
   network <- network1<thres
   return(network)
 }
