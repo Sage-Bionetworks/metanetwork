7c9e5526a7a76a772cafe08d91f971dda95d6e2c
Ben Logsdon <aeberhart@gmail.com>
2015-04-22 12:50:51 -0700

    fix bug in correlation fxn

--- R/correlation.R
+++ R/correlation.R
@@ -1,4 +1,4 @@
-correlation <- function(data,method='pearson'){
+correlation <- function(data,path=NULL,method='pearson'){
   network <- cor(data,method='pearson')
   save(network,file='result_correlation.rda')
 }
