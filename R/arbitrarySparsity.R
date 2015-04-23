fa347779831cef51b21ca020273d575a7721e730
Ben Logsdon <baelogsdon@gmail.com>
2015-04-23 10:38:48 -0700

    document and fix arbitrary sparsity fxn to be protected against loose sparsities

--- R/arbitrarySparsity.R
+++ R/arbitrarySparsity.R
@@ -1,11 +1,20 @@
 arbitrarySparsity <- function(network,nedges){
   require(dplyr)
+  #make the network into a matrix for sure (in case it is stored as a data frame or list)
   network <- network %>% as.matrix
+  #set the diagonal to zero
   diag(network) <- 0
+  #take the absolute value
   network <- network %>% abs
-  require(dplyr)
+  #grab the upper diagonal
   networkVec <- network[network %>% upper.tri %>% which] %>% c
+  #sort the upper diagonal, decreasing!
   sortedNetworkVec<-sort(networkVec,decreasing=T)
+  #need to add a conditional if nedges exceeds the number of non-zero elements
   thres <- sortedNetworkVec[nedges]
-  return(network>thres)
+  if(thres==0){
+    return(NA)
+  }else{
+    return(network>thres)
+  }
 }
