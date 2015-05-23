30a39c2f4a6a91b78afa68fe9b7b9a73257c7527
Ben Logsdon <aeberhart@gmail.com>
2015-05-23 16:36:03 -0700

    fix issues with a ranked edge list

--- R/rankedEdgeList.R
+++ R/rankedEdgeList.R
@@ -1,6 +1,17 @@
-rankedEdgeList <- function(network){
+rankedEdgeList <- function(network,symmetric=FALSE){
   require(dplyr)
-  whichMatrix <- ((network%>%abs) > 0) %>% which(T)
-  internal <- function(ind,x){ return(x[ind[1],ind[2])}
-  cbind(rownames(network)[whichMatrix[,1]],colnames(network)[whichMatrix[,2]],apply(whichMatrix,1,internal,network)) %>% return
+  if(!symmetric){
+    whichMatrix <- ((network%>%abs) > 0) %>% which(T)
+  }else {
+    whichMatrix <- ((network%>%abs) > 0 & network%>%upper.tri) %>% which(T)    
+  }
+  internal <- function(ind,x){ return(x[ind[1],ind[2]])}
+  rankedEdgeList <- cbind(rownames(network)[whichMatrix[,1]],colnames(network)[whichMatrix[,2]],apply(whichMatrix,1,internal,network) %>%abs)
+  colnames(rankedEdgeList) <- c('var1','var2','value')
+  rownames(rankedEdgeList) <- paste0('edge',1:nrow(rankedEdgeList))
+  rankedEdgeList <- rankedEdgeList %>% data.frame(stringsAsFactors = F)
+  rankedEdgeList$value <- as.numeric(rankedEdgeList$value)
+  rankedEdgeList <- rankedEdgeList[order(rankedEdgeList$value,decreasing=T),]
+  rownames(rankedEdgeList) <- paste0('edge',1:nrow(rankedEdgeList))
+  rankedEdgeList %>% return
 }
