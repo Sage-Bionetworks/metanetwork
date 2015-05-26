54755f5fb6cef54d46aeea7505b8f7ba8a5b07d9
Ben Logsdon <aeberhart@gmail.com>
2015-05-26 10:53:37 -0700

    keep sign of edges

--- R/rankedEdgeList.R
+++ R/rankedEdgeList.R
@@ -6,12 +6,12 @@ rankedEdgeList <- function(network,symmetric=FALSE){
     whichMatrix <- ((network%>%abs) > 0 & network%>%upper.tri) %>% which(T)    
   }
   internal <- function(ind,x){ return(x[ind[1],ind[2]])}
-  rankedEdgeList <- cbind(rownames(network)[whichMatrix[,1]],colnames(network)[whichMatrix[,2]],apply(whichMatrix,1,internal,network) %>%abs)
+  rankedEdgeList <- cbind(rownames(network)[whichMatrix[,1]],colnames(network)[whichMatrix[,2]],apply(whichMatrix,1,internal,network))
   colnames(rankedEdgeList) <- c('var1','var2','value')
   rownames(rankedEdgeList) <- paste0('edge',1:nrow(rankedEdgeList))
   rankedEdgeList <- rankedEdgeList %>% data.frame(stringsAsFactors = F)
   rankedEdgeList$value <- as.numeric(rankedEdgeList$value)
-  rankedEdgeList <- rankedEdgeList[order(rankedEdgeList$value,decreasing=T),]
+  rankedEdgeList <- rankedEdgeList[order((rankedEdgeList$value %>% abs),decreasing=T),]
   rownames(rankedEdgeList) <- paste0('edge',1:nrow(rankedEdgeList))
   rankedEdgeList %>% return
 }
