1781fcf728d37fba23c839ebc4f2a722ed42bdd6
Ben Logsdon <aeberhart@gmail.com>
2015-07-22 10:07:32 -0700

    make the connection stronger

--- R/makeModuleNetwork.R
+++ R/makeModuleNetwork.R
@@ -1,7 +1,15 @@
 makeModuleNetwork <- function(modules,net){
   #fxn to convert a network into a collapsed module network
-  for (i in 1:length(modules)){
+  lm <- length(modules)
+  for (i in 1:lm){
     net[modules[[i]],modules[[i]]] <- TRUE
+  }
+  for (i in 1:(lm-1)){
+    for (j in i:lm){
+      if(sum(net[modules[[i]],modules[[j]]])>0){
+        net[modules[[i]],modules[[j]]] <- TRUE
+      }
+    }
   }
   diag(net) <- FALSE
   return(net)
