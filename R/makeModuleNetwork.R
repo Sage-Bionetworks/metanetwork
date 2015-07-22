475297b684215000a33d2bc4eb5bc4410d407164
Ben Logsdon <aeberhart@gmail.com>
2015-07-22 11:05:16 -0700

    make it hierarchical

--- R/makeModuleNetwork.R
+++ R/makeModuleNetwork.R
@@ -1,16 +1,20 @@
 makeModuleNetwork <- function(modules,net){
   #fxn to convert a network into a collapsed module network
   lm <- length(modules)
-  for (i in 1:lm){
-    net[modules[[i]],modules[[i]]] <- TRUE
-  }
+  moduleNetwork <- matrix(0,lm,lm)
+  rownames(moduleNetwork) <- paste0('m',1:lm)
+  colnames(moduleNetwork) <- paste0('m',1:lm)
+  
+  #for (i in 1:lm){
+  #  net[modules[[i]],modules[[i]]] <- TRUE
+  #}
   for (i in 1:(lm-1)){
-    for (j in i:lm){
+    for (j in (i+1):lm){
       if(sum(net[modules[[i]],modules[[j]]])>0){
-        net[modules[[i]],modules[[j]]] <- TRUE
+        moduleNetwork[i,j] <- TRUE
       }
     }
   }
-  diag(net) <- FALSE
-  return(net)
+  #diag(net) <- FALSE
+  return(moduleNetwork)
 }
