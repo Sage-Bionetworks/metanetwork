5ce2de1bfbef92f550c970ac044cd3080bdb8440
Thanneer Malai Perumal <th.vairam@gmail.com>
2015-08-22 05:19:13 +0000

    Added differential expression lists to the AD list

--- R/curateADGeneSets.R
+++ R/curateADGeneSets.R
@@ -41,7 +41,7 @@ setnames(human_ensg2symbol, 'ensembl_gene_id', 'hsapiens_homolog_ensembl_gene')
 mouse_human_mapping = merge(mouse_mapping, human_ensg2symbol, by = 'hsapiens_homolog_ensembl_gene', all=T)
 
 GeneSets_mouse = lapply(MG, function(x, mouse_human_mapping){  
-  entrz_id = x$EntrezGene[(x$FDR <= 0.05 & abs(x[,colnames(x) %in% c('foldchange','FDR')]) >= 1.5)]  
+  entrz_id = x$EntrezGene[(x$FDR <= 0.05 & abs(x[,colnames(x) %in% c('foldchange','FC_ratio')]) >= 1.5)]  
   gs = unique(mouse_human_mapping$hgnc_symbol[mouse_human_mapping$entrezgene %in% entrz_id])
   gs = gs[!is.na(gs)]
   return(gs)
@@ -59,7 +59,24 @@ tmp = fread(synGet('syn4891675')@filePath, data.table=F, header=F)
 ALL_USED_IDs = c(ALL_USED_IDs, 'syn4891675')
 GeneSets_GeneticLoci = list("AD:GeneticLoci" = tmp$V1)
 
-GeneSets = c(GeneSets_CM, GeneSets_GeneticLoci, GeneSets_mouse)
+FDR = fread(synGet('syn4239887')@filePath, data.table=F, header=T)
+FC = fread(synGet('syn4239883')@filePath, data.table=F, header = T)
+
+vals = FDR$ensembl_gene_id[FDR[,'ROSMAP.RNASeq.PrefrontalCortex.cogdx1-cogdx4'] <= 0.05]
+human_ensg2symbol = getBM(attributes = c("ensembl_gene_id","hgnc_symbol"),
+                          filters = "ensembl_gene_id",                         
+                          values = vals,
+                          mart = Hs)
+ROSMAP.NCIvsAD = unique(human_ensg2symbol$hgnc_symbol)
+
+vals = FDR$ensembl_gene_id[FDR[,'ROSMAP.RNASeq.PrefrontalCortex.cogdx2-cogdx4'] <= 0.05]
+human_ensg2symbol = getBM(attributes = c("ensembl_gene_id","hgnc_symbol"),
+                          filters = "ensembl_gene_id",                         
+                          values = vals,
+                          mart = Hs)
+ROSMAP.MCIvsAD = unique(human_ensg2symbol$hgnc_symbol)
+
+GeneSets = c(GeneSets_CM, GeneSets_GeneticLoci, GeneSets_mouse, list(ROSMAP.NCIvsAD = ROSMAP.NCIvsAD, ROSMAP.MCIvsAD = ROSMAP.MCIvsAD))
 
 thisFileName <- 'curateADGeneSets.R'
 
