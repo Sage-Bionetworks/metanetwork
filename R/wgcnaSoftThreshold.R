f8f160e9c256a17654051f1f56b2cb0b644cd348
Benjamin Logsdon <aeberhart@gmail.com>
2016-02-08 10:06:35 -0800

    updated wgcna fxns

--- R/wgcnaSoftThreshold.R
+++ R/wgcnaSoftThreshold.R
@@ -1,7 +1,11 @@
-wgcnaSoftThreshold <- function(data,path=NULL,outputpath,RsquaredCut=.80){
+wgcnaSoftThreshold <- function(data,path=NULL,outputpath,RsquaredCut=.80,defaultNaPower=6){
   library(WGCNA)
   res <- WGCNA::pickSoftThreshold(data,RsquaredCut=RsquaredCut)
+  if(is.na(res$powerEstimate)){
+    res$powerEstimate<-defaultNaPower
+  }
+  
   network <- abs(cor(data))^res$powerEstimate
-  #save(network,file='result_wgcnaSoftThreshold.rda')
+
   save(network,file=paste0(outputpath,'result_wgcnaST.rda'))
 }
--- R/wgcnaTOM.R
+++ R/wgcnaTOM.R
@@ -1,9 +1,12 @@
-wgcnaTOM <- function(data,path=NULL,outputpath,RsquaredCut=.80){
+wgcnaTOM <- function(data,path=NULL,outputpath,RsquaredCut=.80,defaultNaPower=6){
   library(WGCNA)
   res <- WGCNA::pickSoftThreshold(data,RsquaredCut=RsquaredCut)
+  if(is.na(res$powerEstimate)){
+    res$powerEstimate<-defaultNaPower
+  }
   network <- abs(cor(data))^res$powerEstimate
   #save(network,file='result_wgcnaSoftThreshold.rda')
-  
+  print(res$powerEstimate)
   network <- TOMsimilarity(network)
   save(network,file=paste0(outputpath,'result_wgcnaTOM.rda'))
 }
