d91f7229abd31f85231e9016a76081c74f954545
Benjamin Logsdon <aeberhart@gmail.com>
2016-02-11 12:30:28 -0800

    update for live testing

--- exampleCorrMISubmissionsScript.sh
+++ exampleCorrMISubmissionsScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #number of cores to reserve for job
-nthreads=16
+nthreads=2
 
 #full s3 path where networks will go
 s3="s3://metanetworks/testNetwork/"
@@ -33,5 +33,7 @@ errorOutput="/shared/testNetwork/error.txt"
 #path to out output
 outOutput="/shared/testNetwork/out.txt"
 
+
+echo "qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,c3net=1,mrnet=1,aracne=1,wgcnaST=1,wgcnaTOM=1,sparrowZ=0,lassoCV1se=0,ridgeCV1se=0,genie3=0,tigress=0,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe orte $nthreads -S /bin/bash -V -cwd -N testingNetworkFunctionality -e $errorOutput -o $outOutput $pathv/buildNet.sh"
 
 qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,c3net=1,mrnet=1,aracne=1,wgcnaST=1,wgcnaTOM=1,sparrowZ=0,lassoCV1se=0,ridgeCV1se=0,genie3=0,tigress=0,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe orte $nthreads -S /bin/bash -V -cwd -N testingNetworkFunctionality -e $errorOutput -o $outOutput $pathv/buildNet.sh
--- exampleRegressionSubmissionsScript.sh
+++ exampleRegressionSubmissionsScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #number of cores to reserve for job
-nthreads=319
+nthreads=2
 
 #full s3 path where networks will go
 s3="s3://metanetworks/testNetwork/"
@@ -33,5 +33,7 @@ errorOutput="/shared/testNetwork/error.txt"
 #path to out output
 outOutput="/shared/testNetwork/out.txt"
 
+
+echo "qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,c3net=0,mrnet=0,aracne=0,wgcnaST=0,wgcnaTOM=0,sparrowZ=1,lassoCV1se=1,ridgeCV1se=1,genie3=1,tigress=1,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe orte $nthreads -S /bin/bash -V -cwd -N testingNetworkFunctionality -e $errorOutput -o $outOutput $pathv/buildNet.sh"
 
 qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,c3net=0,mrnet=0,aracne=0,wgcnaST=0,wgcnaTOM=0,sparrowZ=1,lassoCV1se=1,ridgeCV1se=1,genie3=1,tigress=1,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe orte $nthreads -S /bin/bash -V -cwd -N testingNetworkFunctionality -e $errorOutput -o $outOutput $pathv/buildNet.sh
