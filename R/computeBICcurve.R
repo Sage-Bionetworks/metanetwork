449b435d774bc2139cf87edf091c70222f2e1a4b
Benjamin Logsdon <aeberhart@gmail.com>
2016-04-11 11:51:54 -0700

    add in additional flags

--- R/computeBICcurve.R
+++ R/computeBICcurve.R
@@ -4,23 +4,31 @@ computeBICcurve <- function(network,exprData,maxEdges=NULL,exact=NULL){
     maxEdges <- round((nrow(exprData)*ncol(exprData))/20)
   }
   maxEdges <- min(maxEdges,round((nrow(exprData)*ncol(exprData))/20))
-
+  cat('maxEdges:',maxEdges,'\n')
   foo <- data.matrix(network)[which(upper.tri(data.matrix(network)))]
+  cat('organize foo\n')
   foo <- abs(foo)
+  cat('set lower triangular to 0\n')
   network[which(lower.tri(network))]<-0
+  cat('set diagonal to zero\n')
   diag(network) <- 0
+  cat('thresVal:',thresVal,'\n')
   thresVal <- sort(foo,decreasing=T)[min(maxEdges,length(foo))]
 
   #add in check for zero edges
   if(thresVal==0){
     thresVal <- min(foo[which(foo>0)])
   }
-
+  cat('network absolution\n')
   network <- abs(network)
+  cat('get edges\n')
   edgeList <- which(network >=thresVal,T)
   edval <- rep(0,nrow(edgeList))
 
   for (i in 1:nrow(edgeList)){
+    if(i%%1000==0){
+      cat(edval[i],'\n')
+    }
     edval[i] <- network[edgeList[i,1],edgeList[i,2]]
   }
 
@@ -29,6 +37,7 @@ computeBICcurve <- function(network,exprData,maxEdges=NULL,exact=NULL){
   rownames(edgeList) <- paste0('e',1:nrow(edgeList))
   edgeList <- data.frame(edgeList,stringsAsFactors=F)
   edgeList <- arrange(edgeList,desc(weight))
+  print(edgeList[1:5,])
   bicPath <- metanetwork::covarianceSelectionMBPath(data.matrix(exprData),rankedEdges=edgeList[,1:2],start=1)
   bicPath2 <- NA;
 
