6c212243e1eaed1393229d1a094f09545d75b6b1
Ben Logsdon <aeberhart@gmail.com>
2017-03-08 16:10:53 -0800

    compare sparse networks fxn

---
+++ R/compareTwoSparseNetworks.R
@@ -0,0 +1,25 @@
+compareTwoSparseNetworks <- function(network1,network2){
+  #get networks into the same format
+  geneName1 <- colnames(network1)
+  geneName2 <- colnames(network2)
+  
+  geneName <- intersect(geneName1,geneName2)
+  
+  network1 <- network1[geneName,geneName]
+  network2 <- network2[geneName,geneName]
+  
+  net1 <- network1[which(upper.tri(network1))]
+  net2 <- network2[which(upper.tri(network2))]
+  
+  #compute edge overlaps: OR, p-value from hypergeometric, along with percent overlap
+  table1 <- table(net1!=0,net2!=0)
+  
+  model <- list()
+  model$fisher <- fisher.test(table1)
+  
+  model$network1Overlap <- table1[1,1]/(rowSums(table1)[1])
+  model$network2Overlap <- table1[2,2]/(rowSums(table1)[2])
+  
+  return(model)
+    
+}
