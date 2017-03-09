387637197fd4f570ffe5ce4f176834e78d1b8637
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-03-09 13:39:50 -0800

    updated comparison

--- R/compareTwoNetworks.R
+++ R/compareTwoNetworks.R
@@ -2,21 +2,15 @@ compareTwoNetworks <- function(network1,network2){
   #get networks into the same format
   geneName1 <- colnames(network1)
   geneName2 <- colnames(network2)
-  
   geneName <- intersect(geneName1,geneName2)
-  
   network1 <- network1[geneName,geneName]
   network2 <- network2[geneName,geneName]
-  
   net1 <- network1[which(upper.tri(network1))]
-  net2 <- network2[which(upper.tri(netw))]
-  
-  #compute edge overlaps: OR, p-value from hypergeometric, along with percent overlap
-  
-  
-  
-  #compute intersection network
-  
-  #make degree distribution plots: histogram and scatterplots of degree scores with correlations
-    
+  net2 <- network2[which(upper.tri(network2))]
+  table1 <- table(net1!=0,net2!=0)
+  model <- list()
+  model$fisher <- fisher.test(table1)
+  model$overlapNet1 <- table1[1,1]/(rowSums(table1)[1])
+  model$overlapNet2 <- table1[2,2]/(rowSums(table1)[2])
+  return(model)
 }
