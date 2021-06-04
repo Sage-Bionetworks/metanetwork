b4a3dfda08dd3bace0bf125b6c7f05802f03de0e
Pradeep Varathan <pradluzog@gmail.com>
2021-06-04 11:13:43 -0400

    Update installAracne.R

--- R/installAracne.R
+++ R/installAracne.R
@@ -1,21 +1,17 @@
 #install aracne function
 installAracne <- function(path=NULL){
-  require(synapseClient)
-  synapseLogin()
+
   cwd <- getwd()
-  aracne <- synGet(id='syn3076450')
-  if(!is.null(path)){
-    setwd(path)
-  }
-  system(paste('tar -xzvf ',aracne@filePath,sep=''))
+  if(dir.exists('~/ARCANE'){
     #str1 <- aracne@filePath;
   #str2 <- strsplit(str1,'ARACNE.src.tar.gz')[[1]]
-  str3 <- 'ARACNE/'
-  cat(str3,'\n')  
-  setwd(str3)
-  #system(paste(str2,'/ARACNE.src/ARACNE/',sep=''))
-  system('make')
-  #setwd('../')
-  setwd(cwd)
-  #return(paste(getwd(),'/ARACNE/',sep=''))
+    str3 <- '~/ARACNE/'
+    cat(str3,'\n')  
+    setwd(str3)
+    #system(paste(str2,'/ARACNE.src/ARACNE/',sep=''))
+    system('make')
+    #setwd('../')
+    setwd(cwd)
+    #return(paste(getwd(),'/ARACNE/',sep=''))
+    }
 }
