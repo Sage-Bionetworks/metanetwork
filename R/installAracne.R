0a7e6dba6e1633ee72213089617e8fd9b248aabb
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-06-07 14:45:30 -0400

    Update installAracne.R

--- R/installAracne.R
+++ R/installAracne.R
@@ -1,7 +1,7 @@
 installAracne <- function(path=NULL){
   arc_path = system.file("tools", "ARACNE.src.tar.gz", package = "metanetwork")
   system(paste('tar -xzvf ',arc_path,sep=''))
-    #str1 <- aracne@filePath;
+  #str1 <- aracne@filePath;
   #str2 <- strsplit(str1,'ARACNE.src.tar.gz')[[1]]
   str3 <- 'ARACNE/'
   cat(str3,'\n')  
