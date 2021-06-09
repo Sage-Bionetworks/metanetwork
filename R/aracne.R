593b1cc08b8301e9bc368d8c6a27905dcfd6678e
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-06-09 11:08:17 -0400

    Update aracne.R

--- R/aracne.R
+++ R/aracne.R
@@ -1,18 +1,25 @@
 ###Function to run aracne on the data
-aracne <- function(data,path=NULL,pval=NULL,outputpath){
+aracne <- function(data,path=NULL,pval=NULL,outputpath,na_fill = NULL){
   #path is the a string of the path to th aracne compiled executable
   #data is a matrix of the gene expression data of interest
-  data <- t(data);
   installAracne()
-
+  library(caroline)
   if(is.null(pval)){
     pval <- 0.05/choose(nrow(data),2)
   }
+  if(is.null(na_fill)){
+    data <- na.omit(data)
+  }
+  else{
+    data[is.na(data)] <- na_fill # Ideally, user could insert a large negative number or use min(data)
+  }
   temp_path = paste0(config$input_profile$temp_storage_loc,"/ARACNE")
   setwd(temp_path)
+
+  
   dataMatrix <- cbind(rownames(data),rownames(data),data)
   colnames(dataMatrix) <- c('name1','name2',colnames(data))
-  write.table(dataMatrix,file='dataMatrix.tsv',sep='\t',quote=FALSE,row.names=FALSE)
+  caroline::write.delim(dataMatrix,file='dataMatrix.tsv',sep='\t',quote=FALSE,row.names=FALSE)
   
   command_string <- paste('./aracne2 -i dataMatrix.tsv -a adaptive_partitioning -p ',pval,' -o result.out',sep='')
   system(command_string) 
