0aeb66591c4e26e42aa4b452893259de44135a7d
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2021-07-16 13:08:28 -0400

    New functions for consensus wrapper

---
+++ R/buildConsensus.R
@@ -0,0 +1,52 @@
+
+buildConsensus = function(outputpath,networkFolderId, fileName, pattern_id){
+  
+  #get all networks from Synapse
+  bar <- synGetFiles(networkFolderId, downloadLocation = outputpath, pattern_id = pattern_id)
+  
+  loadNetwork <- function(file){
+    sparrowNetwork <- data.table::fread(file,stringsAsFactors=FALSE,data.table=F)
+    rownames(sparrowNetwork) <- sparrowNetwork$V1
+    sparrowNetwork <- sparrowNetwork[,-1]
+    gc()
+    return(sparrowNetwork)
+  }
+  getPaths <- function(bar){
+    names <- c()
+    for (i in 1:length(bar)){
+      temp <- bar[[i]]$path
+      names <- append(names,temp)
+    }
+    return(names)
+  }
+  
+  networkFiles <- getPaths(bar)
+  networks <- lapply(networkFiles,loadNetwork)
+  networks <- lapply(networks,data.matrix)
+  
+  networks$rankConsensus <- metanetwork::rankConsensus(networks)
+  cat('built rank consensus\n')
+  cat('write rank consensus\n')
+  write.csv(networks$rankConsensus,file=paste0(outputpath,'rankConsensusNetwork.csv'),quote=F)
+  if(!is.null(fileName)){
+    library(Matrix)
+    networkMethods <- sapply(bar,synGetAnnotation,which='method')
+    cat('grabbed methods\n')
+    #build rank consensus
+    cat('updated methods\n')
+    networkMethods <- c(networkMethods,'rankConsensus')
+    cat('reading in data\n')
+    options(stringsAsFactors = F)
+    dataSet <- reader(fileName, row.names=1)
+    cat('turning data into data matrix\n')
+    dataSet <- data.matrix(dataSet)
+    cat('build bicNetworks\n')
+    #bicNetworks <- lapply(networks,metanetwork::computeBICcurve,dataSet,maxEdges=1e5)
+    bicNetworks <- metanetwork::computeBICcurve(networks$rankConsensus,dataSet,maxEdges=2e5)
+    #cat('make names of bicNetworks\n')
+    #names(bicNetworks) <- 'rankConsensus'
+    cat('save bicNetworks\n')
+    save(bicNetworks,file=paste0(outputpath,'bicNetworks.rda'))
+  }
+  
+}
---
+++ R/synGetFiles.R
@@ -0,0 +1,17 @@
+synGetFiles <- function(project_id, pattern_id, downloadLocation = getwd()){
+  
+  child_obj <- synapser::synGetChildren(project_id)              
+  child_list <- child_obj$asList()
+  child_names <- lapply(child_list, `[[`, 1)
+  child_names <- unlist(child_names)
+  child_names_ids <- grep(pattern_id, child_names)
+  out_list <- c()
+  # [1] "Filtered Spearman Correlation Table"
+  for( ent in child_names_ids){
+    message(child_list[[ent]]$name)
+    temp <- synGet(child_list[[ent]]$id, downloadLocation =downloadLocation)
+    out_list <- append(out_list, temp)
+  }
+  print("Downloaded all required network files")
+  return(out_list)
+}
