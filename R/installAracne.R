e4ce27cba4ac77e1b748d6df0814b130fb87f609
Pradeep Varathan <pradeepvarathanpugalenthi@gmail.com>
2022-03-03 16:18:40 -0500

    Update installAracne.R

--- R/installAracne.R
+++ R/installAracne.R
@@ -4,16 +4,18 @@
 #' 
 #' @param path Optional. Path is currently not a functional argument. This function
 #' automatically installs ARACNe from `inst/tools`. Default = NULL.
-#' 
+#' tool_storage_loc Required. Provides the directory inside docker to 
+#' temporarily store the ARACNE files and package 
+#'
 #' @export
 #' @return NULL
 #' 
-installAracne <- function(path=NULL){
+installAracne <- function(path=NULL,tool_storage_loc = config$input_profile$temp_storage_loc){
   arc_path = system.file("inst/tools", "ARACNE.src.tar.gz", package = "metanetwork")
   system(paste('tar -xzvf ',arc_path,sep=''))
   #str1 <- aracne@filePath;
   #str2 <- strsplit(str1,'ARACNE.src.tar.gz')[[1]]
-  temp_path = paste0(config$input_profile$temp_storage_loc,"/ARACNE")
+  temp_path = paste0(tool_storage_loc,"/ARACNE")
   setwd(temp_path)
   cat(temp_path,'\n')  
   setwd(temp_path)
